95f0ece21a72a0589f90d01f21d90cdb537a1cb0
InsideOutElephant <skippy.bb@gmail.com>
2019-05-06 00:15:30 +0100

    Custom logger implemented

--- src/log/MyLogger.java
+++ src/log/MyLogger.java
@@ -1,6 +1,39 @@
 package log;
 
 
-class MyLogger  {
+import java.io.IOException;
+import java.util.logging.*;
 
+public class MyLogger  {
+    static private FileHandler fileTxt;
+    static private SimpleFormatter formatterTxt;
+
+    static private FileHandler fileHTML;
+    static private Formatter formatterHTML;
+
+    static public void setup() {
+
+        // get the global logger to configure it
+        Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+        // suppress the logging output to the console
+//        Logger rootLogger = Logger.*getLogger*("");
+//        Handler[] handlers = rootLogger.getHandlers();
+//        if (handlers[0] instanceof ConsoleHandler) {
+//            rootLogger.removeHandler(handlers[0]);
+//        }
+
+        logger.setLevel(Level.ALL);
+        try {
+            fileTxt = new FileHandler("C:/test/AppEx/log/Logging.txt", 8096, 1, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // create a TXT formatter
+        formatterTxt = new SimpleFormatter();
+        fileTxt.setFormatter(formatterTxt);
+        logger.addHandler(fileTxt);
+    }
 }
+
